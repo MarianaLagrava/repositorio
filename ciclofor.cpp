0dfe2bd901006572ae20dcabba11ba42589ac6ea
MarianaLagrava <00219820@uca.edu.sv>
2020-10-01 22:23:56 -0600

    Hola

--- ciclofor.cpp
+++ ciclofor.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(void){
     int i;
     for(i=5;i>0;i--){
-        cout << "i="<<i<<endl;
+        cout << "i="<<i<<endl; 
     }
     cout<<"valor final i ="<<i<<endl;
 return 0;
-}
+}//asdasdd
